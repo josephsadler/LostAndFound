689e035d3492fde7fb04734d41aac0cac13778eb
josephsadler <jsadle5@students.towson.edu>
2017-11-30 13:15:03 -0500

    Changed search to output in buttons
    Buttons still needs to be aligned.
    Also added images for the items in the script

--- LostAndFound/script.sql
+++ LostAndFound/script.sql
@@ -25,11 +25,11 @@ insert into users (userName, pass)
 values ('admin', sha2('admin', 256));
 
 insert into items (description, imgPath, turnedIn, location)
-values ('Set of Ford keys', 'tbd', '2017-11-15', 'York'), 
-	   ('Brown leather wallet', 'tbd', '2017-11-1', 'Smith'), 
-	   ('Orioles baseball hat', 'tbd', '2017-11-3', 'Union'),
-       ('Samsung phone with black case', 'tbd', '2017-11-4', 'York'),
-       ('Beige/Tan jacket', 'tbd', '2017-11-5', 'Van Bokkelen'),
-       ('Silver Acer laptop', 'tbd', '2017-11-14', 'Liberal Arts'),
-       ('Black briefcase with a silver lock', 'tbd', '2017-11-9', 'Cook Library'),
-       ('Black Nike hoodie', 'tbd', '2017-11-5', 'Union');
+values ('Set of Ford keys', 'keyfob.jpg', '2017-11-15', 'York'), 
+	   ('Brown leather wallet', 'wallet.jpg', '2017-11-1', 'Smith'), 
+	   ('Orioles baseball hat', 'hat.jpg', '2017-11-3', 'Union'),
+       ('Samsung phone with black case', 'samsung.jpg', '2017-11-4', 'York'),
+       ('Beige/Tan jacket', 'jacket.jpg', '2017-11-5', 'Van Bokkelen'),
+       ('Silver Acer laptop', 'Acer.jpg', '2017-11-14', 'Liberal Arts'),
+       ('Black briefcase with a silver lock', 'briefcase.jpg', '2017-11-9', 'Cook Library'),
+       ('Black Nike hoodie', 'hoodie.jpg', '2017-11-5', 'Union');
--- LostAndFound/search.php
+++ LostAndFound/search.php
@@ -22,23 +22,39 @@ $numRows = count($results);
 if ($numRows == 0) {
     echo 'No results found';
 } else {
-    echo $numRows . " results found for '" . $search . "'";
-    echo '
-                <table>
-                    <tr>
-                        <th>Image</th>
-                        <th>Description</th>
-                        <th>Date Recovered</th> 
-                        <th>Location</th> 
-                    </tr>';
+    /*
+      echo $numRows . " results found for '" . $search . "'";
+      echo '
+      <table>
+      <tr>
+      <th>Image</th>
+      <th>Description</th>
+      <th>Date Recovered</th>
+      <th>Location</th>
+      </tr>';
 
+      while ($row = mysqli_fetch_array($results)) {
+      echo '<tr><td>', $row[2], '</td>
+      <td>', $row[1], '</td>
+      <td>', $row[3], '</td>
+      <td>', $row[4], '</td></tr>';
+      }
+      echo '</table>';
+     */
+
+    $rowNum = 1;
+    
     while ($row = mysqli_fetch_array($results)) {
-        echo '<tr><td>', $row[2], '</td>
-                          <td>', $row[1], '</td>   
-                          <td>', $row[3], '</td>
-                          <td>', $row[4], '</td></tr>';
+        echo '<div class ="row justify-content-between" align="center">
+            <a class ="col-3 btn btn-primary" href="#" role="button" id="img' . $rowNum . '" onclick="dibsPopUp1()">
+               <span class="dibs" id="dibs1">Click me to dibs</span>
+               <div>';
+        echo '<img src=' . $row[2] . ' alt = "Could not load image" height="195" width="195">
+                <p><br>'. $row[1] . '</p>
+              </div></a>';
+        
+        $rowNum = $rowNum + 1;
     }
-    echo '</table>';
 }
 ?>
 
